26e3db3d89a398767857eb39eac904866419c140
Ginger-Mano <mcasimirian@gmail.com>
2020-08-03 20:52:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code heremain
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Manon is the champion'
